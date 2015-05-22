ab4d26549c7a96ff0b58e158a2d3216868d919f2
Neo <neo.choi@gmail.com>
2015-05-23 02:23:16 +0800

    store data in db directly

--- main.js
+++ main.js
@@ -25,12 +25,27 @@ function getStudentName(pId) {
 				var str = iconv.decode(new Buffer(body), "big5");
 				var $ = cheerio.load(str);
 				if ($("#context .portrait-profile img").length > 0) {
+					student._id = pId;
 					student.gender = $("#context .portrait-profile img").attr("src").indexOf("f.gif") != -1 ? "F" : "M";
 					student.name = $("#content2 tr").eq(2).find("td").contents().eq(2).text();
 					student.engName = $("#content2 tr").eq(1).find("td").eq(1).contents().eq(2).text();
-					if (student.name === "--") student = {};
+					if (student.engName === "--") student.engName = "";
+					if (student.name === "--") {
+						student = {};
+						deferred.resolve(student);
+					}
+					else {
+						MongoClient.connect('', function(err, db) {
+							if(err) throw err;
+							//console.log('connected')
+							db.collection('student').insert(student, {w:1});
+							db.close();
+							deferred.resolve(student);
+						});
+					}
 				}
-				deferred.resolve(student);
+				else 
+					deferred.resolve(student);
 			} catch (err) {
 				deferred.resolve(student);
 			}
@@ -47,25 +62,10 @@ for (var i = start; i <= end; i++) {
 }
 
 Q.all(list.map(getStudentName))
+	/*
 .spread(function() {
 	var data = arguments;
 	console.log(data);
-	MongoClient.connect('', function(err, db) {
-		if(err) throw err;
-		console.log('connected')
-		for(var i = 0; i < data.length; i++) {
-			if (!data[i].hasOwnProperty("name")) continue;
-			data[i]._id = data[i].name;
-			console.log(data[i]);
-			db.collection('student').insert(data[i], {w:1}, function(err) {
-				//console.log('callback')
-				//if (err) console.warn(err.message);
-				//else console.log('successfully inserted ' + data[i]);
-			});
-		}
-		db.close();
-	});
-	/*
-*/
 })
+*/
 .done();
