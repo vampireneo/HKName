96704418f28451e99c87710607306aa9a6b5b1e7
Neo <neo.choi@gmail.com>
2015-07-03 22:48:36 +0800

    keep trying

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 #HK NAME
+[![Build Status](https://travis-ci.org/vampireneo/HKName.svg?branch=master)](https://travis-ci.org/vampireneo/HKName)
 [![Code Climate](https://codeclimate.com/github/vampireneo/HKName/badges/gpa.svg)](https://codeclimate.com/github/vampireneo/HKName)
 [![Coverage Status](https://coveralls.io/repos/vampireneo/HKName/badge.svg)](https://coveralls.io/r/vampireneo/HKName)
 
--- test/test.js
+++ test/test.js
@@ -1,9 +1,20 @@
 var assert = require("assert"); // node.js core module
+var reader = require("../reader.js");
 
 describe('Array', function(){
   describe('#indexOf()', function(){
     it('should return -1 when the value is not present', function(){
       assert.equal(-1, [1,2,3].indexOf(4)); // 4 is not present in this array so indexOf returns -1
-    })
-  })
+    });
+  });
+});
+
+describe('reader.js', function(){
+  describe('getData()', function(){
+    it('should no error when calling getDate()', function(){
+      var list = [2];
+      reader.getData(list);
+      assert.equal(1, 1);
+    });
+  });
 });
